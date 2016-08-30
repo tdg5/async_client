37f47e23a97c2f3c99dab8cd6fdf5d53aff92d37
Danny Guinther <dannyguinther@gmail.com>
2016-08-30 19:07:02 -0400

    Add factory methods to AsyncClient::Clients::Qless

--- lib/async_client/clients/qless.rb
+++ lib/async_client/clients/qless.rb
@@ -2,3 +2,20 @@ require "async_client/client"
 require "qless"
 require "async_client/clients/qless/client"
 require "async_client/clients/qless/client_factory"
+
+
+module AsyncClient
+  module Clients
+    module Qless
+      def self.build_async_client(host, port)
+        qless_async_client = build_qless_async_client(host, port)
+        AsyncClient::Client.new(qless_async_client)
+      end
+
+      def self.build_qless_async_client(host, port)
+        factory = ClientFactory.new(:host => host, :port => port)
+        factory.product
+      end
+    end
+  end
+end
