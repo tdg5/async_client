1488767a9b6c45cb837d4f87e9656b366cf4e15d
Danny Guinther <dannyguinther@gmail.com>
2016-09-11 01:18:23 -0400

    Autoload RamReclamationJob with Qless client

--- lib/async_client/clients/qless.rb
+++ lib/async_client/clients/qless.rb
@@ -2,7 +2,7 @@ require "async_client"
 require "qless"
 require "async_client/clients/qless/client"
 require "async_client/clients/qless/client_factory"
-
+require "async_client/jobs/qless/ram_reclamation_job"
 
 module AsyncClient
   module Clients
